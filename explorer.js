c1f8dc26387a5f45e8981a05e296eb16487b4d7b
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-06 13:34:36 +0000

    test done

--- explorer.js
+++ explorer.js
@@ -1,11 +1,15 @@
 const Twilio = require("twilio");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const client = new Twilio(
-  "AC122b9715ecd64dc93f9c00a049c0e5fa",
-  "2929765443d4b9dab3362b9d5c9ed731"
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 );
 
 client.messages
   .list()
   .then((messages) =>
     console.log(`The most recent message is ${messages[0].body}`)
-  ).catch((err) => console.log(err));
+  )
+  .catch((err) => console.log(err));
